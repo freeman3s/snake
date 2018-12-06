86293cb4646dfa7f0aaa3e16e7ad89398e8d52d8
max <maxim.diachuk@yahoo.com>
2018-12-06 23:47:00 +0200

    lesson 3

---
+++ Point.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
