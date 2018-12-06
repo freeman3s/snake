9659cbf8d2516f57cd194257c1bbe01d52f85564
max <maxim.diachuk@yahoo.com>
2018-12-06 23:01:09 +0200

    hello

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snake
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
