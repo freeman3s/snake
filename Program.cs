316240cf233ed4d6e8f00aaecc819cd312a0cd5a
max <maxim.diachuk@yahoo.com>
2018-12-07 20:53:10 +0000

    lesson 6 - List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -6,31 +7,26 @@ namespace snake
     {
         public static void Main(string[] args)
         {
-            int x = 1;
-            Func1( x );
-            Console.WriteLine( "Call Func1. x = " + x );
+            Point p1 = new Point(1,3,'*');
+            Point p2 = new Point(4,5,'#');
+            
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-            x = 1;
-            Func2( x );
-            Console.WriteLine( "Call Func2. x = " + x );
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
 
-            x = 1;
-            Func3( x );
-            Console.WriteLine( "Call Func3. x = " + x );
-
-            Point p1 = new Point( 1, 3, '*' );
-            Move( p1, 10, 10 );
-            Console.WriteLine( "Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y );
-
-            Point p2 = new Point( 4, 5, '#' );
-            p1 = new Point( 4, 5, '#' );
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine( "p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = "/* + p2.x + ", p2.y = " + p2.y*/ );
-
-            p1 = new Point( 1, 3, '*' );
-            Update( p1 );
-            Console.WriteLine( "Call Update. p1.x = " + p1.x + ", p1.y = " + p1.y  + ", p1.sym = " + p1.sym );
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
         }
         
         public static void Func1( int value )
