bddfa353189e24db1899bc692762059d8e274c34
max <maxim.diachuk@yahoo.com>
2018-12-07 21:58:42 +0000

    lesson 7 - HorizontalLine

--- Program.cs
+++ Program.cs
@@ -7,56 +7,11 @@ namespace snake
     {
         public static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
+            Point p1 = new Point(1, 3, '*');
             Point p2 = new Point(4,5,'#');
             
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(0, 10, 1, '+');
+            line.Draw();
         }
-        
-        public static void Func1( int value )
-        {
-			
-        }
-
-        public static void Func2( int value )
-        {
-            value = value + 1;
-        }
-
-        public static void Func3( int x )
-        {
-            x = x + 1;
-        }
-
-        public static void Move( Point p, int dx, int dy )
-        {
-            p.x += dx;
-            p.y += dy;
-        }
-
-        public static void Update( Point p )
-        {
-            Point p2 = new Point(0, 0, '.');
-            p = p2;
-            Console.WriteLine( "Call Update. p1.x = " + p.x + ", p1.y = " + p.y  + ", p1.sym = " + p.sym );
-        }
-
-        
     }
 }
